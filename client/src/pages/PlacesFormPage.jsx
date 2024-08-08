490a83087a6b1af2577ab233fbb8160da454e42f
Chantel Carter <chantel.alexia@gmail.com>
2024-08-07 19:18:01 -0700

    user can view list of properties on index page

--- client/src/pages/PlacesFormPage.jsx
+++ client/src/pages/PlacesFormPage.jsx
@@ -16,6 +16,7 @@ export default function PlacesFormPage() {
   const [checkIn, setCheckIn] = useState('')
   const [checkOut, setCheckOut] = useState('')
   const [maxGuests, setMaxGuests] = useState(1)
+  const [price, setPrice] = useState(100)
   const [redirect, setRedirect] = useState(false)
   useEffect(() => {
     if (!id) {
@@ -32,6 +33,7 @@ export default function PlacesFormPage() {
       setCheckIn(data.checkIn)
       setCheckOut(data.checkOut)
       setMaxGuests(data.maxGuests)
+      setPrice(data.price)
     })
   }, [id])
 
@@ -59,7 +61,7 @@ export default function PlacesFormPage() {
     const placeData = {
       title, address, addedPhotos,
       description, perks, extraInfo,
-      checkIn, checkOut, maxGuests
+      checkIn, checkOut, maxGuests, price,
     }
     if (id) {
       await axios.put('/places', {
@@ -105,7 +107,7 @@ export default function PlacesFormPage() {
         {preInput('Extra Information', 'House rules, etc.')}
         <textarea value={extraInfo} onChange={ev => setExtraInfo(ev.target.value)} />
         {preInput('Check-in and Check-out times', 'Add check-in and check-out times. Remember to have a time window for cleaning the room between guests!')}
-        <div className="grid gap-2 sm:grid-cols-3">
+        <div className="grid gap-2 grid-cols-2 md:grid-cols-4">
           <div className="mt-2 -mb-1">
             <h3> Check-in:</h3>
             <input
@@ -130,6 +132,14 @@ export default function PlacesFormPage() {
               type="number"
               value={maxGuests}
               onChange={ev => setMaxGuests(ev.target.value)}
+            />
+          </div>
+          <div className="mt-2 -mb-1">
+            <h3> Price per night:</h3>
+            <input
+              type="number"
+              value={price}
+              onChange={ev => setPrice(ev.target.value)}
             />
           </div>
         </div>
